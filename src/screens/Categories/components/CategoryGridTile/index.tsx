3cbd747f62611404428195a438ae6a5a74af6aee
Washington Igor <washington.igor22@gmail.com>
2024-04-22 21:42:39 -0300

    wip

--- src/screens/Categories/components/CategoryGridTile/index.tsx
+++ src/screens/Categories/components/CategoryGridTile/index.tsx
@@ -1,4 +1,4 @@
-import { Pressable, View, Text } from 'react-native';
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 interface CategoryGridTileProps {
   title: string;
@@ -7,12 +7,60 @@ interface CategoryGridTileProps {
 
 export const CategoryGridTile = ({ title, color }: CategoryGridTileProps) => {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.container}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View
+          style={[
+            styles.content,
+            {
+              backgroundColor: color,
+            },
+          ]}
+        >
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.select({
+      android: "hidden",
+      ios: "visible",
+    }),
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.65,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
--- src/screens/Categories/index.tsx
+++ src/screens/Categories/index.tsx
@@ -1,7 +1,8 @@
-import { FlatList } from 'react-native';
-import { CATEGORIES } from '../../../data/dummy-data';
-import Category from '../../../models/category';
-import { CategoryGridTile } from './components/CategoryGridTile';
+import { FlatList, SafeAreaView } from "react-native";
+import { CATEGORIES } from "../../../data/dummy-data";
+import Category from "../../../models/category";
+import { CategoryGridTile } from "./components/CategoryGridTile";
+import { StatusBar } from "expo-status-bar";
 
 const renderCategory = (category: Category) => {
   return <CategoryGridTile title={category.title} color={category.color} />;
@@ -9,11 +10,15 @@ const renderCategory = (category: Category) => {
 
 export const Categories = () => {
   return (
-    <FlatList
-      data={CATEGORIES}
-      renderItem={({ item }) => renderCategory(item)}
-      keyExtractor={(item) => item.id}
-    />
+    <SafeAreaView>
+      <StatusBar style="light"/>
+      <FlatList
+        data={CATEGORIES}
+        renderItem={({ item }) => renderCategory(item)}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
+    </SafeAreaView>
   );
 };
 
